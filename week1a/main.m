0e32edec8149993b257140dbc595db81e4fbc201
junuki <jungkim@jungkim-macbookpro2.roam.corp.google.com>
2014-06-22 08:50:26 -0700

    Initial Commit

---
+++ week1a/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  week1a
+//
+//  Created by Jung Eun Kim on 6/22/14.
+//  Copyright (c) 2014 jung. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ week1aTests/week1aTests.m
@@ -0,0 +1,34 @@
+//
+//  week1aTests.m
+//  week1aTests
+//
+//  Created by Jung Eun Kim on 6/22/14.
+//  Copyright (c) 2014 jung. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface week1aTests : XCTestCase
+
+@end
+
+@implementation week1aTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
